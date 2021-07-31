1508b055635e6a4a4470be2ec16f252cd67d420b
Rahul Kumar <rahul080924@outlook.com>
2021-07-31 20:43:41 +0530

    clean up code to start with session based login

--- index.js
+++ index.js
@@ -6,9 +6,6 @@ const port = process.env.PORT || 8000;
 const auth = require('./API/Routes/auth');
 const passport = require('passport');
 
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,33 +15,15 @@ app.get('/', (req, res) => {
 });
 
 //DB conncention
-var test24 = mongoose
+mongoose
 	.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then((user) => console.log('Connected'))
 	.catch((err) => console.log('Error ', err));
-// const sessionStore = new MongoStore({
-// 	mongooseConnection: test24,
-// 	collection: 'sessions',
-// });
-app.use(
-	session({
-		//secret: process.env.SECRET,
-		secret: 'some secret',
-		resave: false,
-		saveUninitialized: true,
-		store: MongoStore.create({ mongoUrl: dbUrl }),
-		cookie: {
-			maxAge: 1000 * 30,
-		},
-	}),
-);
+
 // initialize the passport middleware
 app.use(passport.initialize());
-app.use(passport.session());
 
 // config for JWt Strategies
-require('./strategies/jsonwtStrategies')(passport);
-require('./strategies/localStategy')(passport);
 
 // Route
 app.use('/api/auth', auth);
