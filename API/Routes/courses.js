a2d165911809b9f186370eef0af4d0b284d9f109
Rahul Kumar <rahul080924@outlook.com>
2021-08-01 21:16:37 +0530

    buy-course route configured

--- API/Routes/courses.js
+++ API/Routes/courses.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Course = require('../../Models/courses');
+const User = require('../../Models/users');
 const passport = require('passport');
 
 /*
@@ -54,6 +55,70 @@ router.post(
 			.catch((err) =>
 				console.log(
 					'Error occurred while checking the course is already present of not. Route = /api/courses/add-course Method POST',
+					err,
+				),
+			);
+	},
+);
+
+/*
+
+    @ Route Type => Post
+    @ Route Address => '/api/courses/buy-course'
+    @ Route Access => Private
+	@ Description => Responsible for buying course by Student
+
+*/
+router.post(
+	'/buy-course',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		if (req.user.role !== 'student') {
+			return res.json({
+				isSuccess: false,
+				errorMsg: 'Course can brought only by Student',
+			});
+		}
+		Course.findOne({ name: req.body.name, addedBy: req.body.addedBy })
+			.then((course) => {
+				if (!course) {
+					return res.json({
+						isSuccess: false,
+						errorMsg: 'Course is not present in the DB',
+					});
+				}
+				User.findById(req.user._id)
+					.then((user) => {
+						const coursesBrought = user.courses;
+						if (coursesBrought.includes(course._id)) {
+							return res.json({
+								isSuccess: false,
+								errorMsg: 'Course already brought by Student',
+							});
+						}
+						user.courses.push(course._id);
+						course.enrolledBy += 1;
+						course.save();
+						user
+							.save()
+							.then((updatedUser) => {
+								return res.json({
+									isSuccess: true,
+									user: updatedUser,
+								});
+							})
+							.catch((err) => console.log(err));
+					})
+					.catch((err) =>
+						console.log(
+							'Error occurred while checking the User is present . Route = /api/courses/buy-course Method POST',
+							err,
+						),
+					);
+			})
+			.catch((err) =>
+				console.log(
+					'Error occurred while checking the course is already present of not. Route = /api/courses/buy-course Method POST',
 					err,
 				),
 			);
